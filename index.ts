86d03f519d54c0b41ade99a515725baf892ed2a5
HelloIsReal <60082945+HelloIsReal@users.noreply.github.com>
2026-07-15 01:26:58 +1200

    Switched from bot to selfbot. Took way to long 😭

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import { App } from "@slack/bolt";
 
-console.log("started!");
+
 const app = new App({
     token: process.env.SLACK_TOKEN,
     appToken: process.env.SLACK_APP_TOKEN,
     socketMode: true,
 });
-
+console.log("started!");
 app.message(async(event) => {
     if (event.payload.subtype) return;
     if (event.payload.user !== 'U0ATHP3CRGF') // rip doppel
