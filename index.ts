a7001fe6811a2a6a6268dcf447e879a73bed0c3d
HelloIsReal <60082945+HelloIsReal@users.noreply.github.com>
2026-07-17 04:43:59 +1200

    Checks if user sent a word from keywords before sending a message!

--- index.ts
+++ index.ts
@@ -3,29 +3,26 @@ const jokes: readonly string[] = ["Don't marry me! I don't want you as my wi-fi!
 const keywords: string[] = ["joke","humor","humour","funny"];
 var chosenJoke=0;
 
+
 const app = new App({
     token: process.env.SLACK_TOKEN,
     appToken: process.env.SLACK_APP_TOKEN,
     socketMode: true,
 });
 console.log("started!");
-app.message(async ({event, message, say}) => {
-//app.message(async(event, message) => {
-    if (event.payload.subtype) return;
-    if (event.payload.user !== 'U0ATHP3CRGF') // rip doppel
-    console.log("sent reply!");
-    //
+app.message(async ({ message, say}) => {
+    if (!("text" in message)) return;
+    if (message.user !== "U0ATHP3CRGF") return;
 
-    var userInput = 'text' in message ? message.text?.toLowerCase(): '';
-    if(!userInput) return; // runs if there isn't any input
-    var keywordCheck = keywords.find(keyword => userInput.includes(keyword));
-    if(keywordCheck){
-        await event.say("I don't want want you as my wi-fi!!!");
-    }else{
-        await event.say("Yo yo yo");
+    const text = message.text ?? "";
+
+    if (keywords.some(keyword => text.includes(keyword))) {
+    await say("Omg you are a wifi!");
+    return;
+    } else{
+        await say(`You said: ${text}`);
     }
-
-
+    
 });
 
 await app.start();
